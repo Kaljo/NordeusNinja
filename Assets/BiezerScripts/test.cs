1d625519e2d06194ae2686a4a32000b18167485b
Kaljo123 <nkaljo@gmail.com>
2015-09-30 23:32:32 +0200

    shuriken attaching

--- Assets/BiezerScripts/test.cs
+++ Assets/BiezerScripts/test.cs
@@ -3,11 +3,17 @@ using System.Collections;
 
 public class test : MonoBehaviour {
 
-	void OnTriggerEnter(Collider other){
-		Debug.Log ("dsadas");
+	public GameObject shuriken;
+
+	void Update(){
+
+		if (Input.GetMouseButtonDown(0)){
+			GameObject s = (GameObject)Instantiate(shuriken, new Vector3(0, Random.Range(1, 4), 0), Quaternion.identity);
+			s.GetComponent<Rigidbody> ().AddTorque (s.transform.up * 600f);
+			s.GetComponent<Rigidbody>().AddForce(new Vector3(Random.Range(-500, 500),0, 1000));
+		}
+
 	}
 
-	void OnCollisionEnter(Collision collision){
-		Debug.Log ("aaaaaaaaaaaaa");
-	}
+
 }
